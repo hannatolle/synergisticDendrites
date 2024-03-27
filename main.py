d011b824e9a7cc6639316b75a7d200f17e4f3090
Hanna Tolle <hanna.m.tolle@gmail.com>
2024-03-27 10:10:10 +0100

    add: print elapsed time when run is finished

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from itertools import product
 import sys
 import simulations_dendritic_dyn
+from time import time
 
+# stop the time
+start = time()
 
 # Define indices of input synapses (indices of 2 out of 193 dendrite sections)
 params = list(product(range(193), range(193)))
@@ -13,3 +16,8 @@ nn = params[job_id-1]
 
 # Run the simulation and save results
 simulations_dendritic_dyn.run(nn, path2home)
+
+# give output
+stop = time()
+print(f'finished job {job_id} with sections {nn[0]} and {nn[1]}.')
+print(f'elapsed time: {stop-start}')

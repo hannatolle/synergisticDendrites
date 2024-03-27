435d07bf7dc11351af797d63dfc4e079f397abce
Hanna Tolle <hanna.m.tolle@gmail.com>
2024-03-27 13:47:16 +0100

    fix: compiling must be done manually via terminal once before submitting the job

--- job.sh
+++ job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#PBS -l select=1:ncpus=32:mem=32gb
+#PBS -l select=1:ncpus=8:mem=8gb
 #PBS -l walltime=08:00:00
 #PBS -N dend_sims
 #PBS -J 1-2
@@ -9,5 +9,4 @@ module load anaconda3/personal
 source activate py311
 
 cd ~/projects/synergisticDendrites/
-nrnivmodl L5bPCmodelsEH/mod/
 python main.py ${HOME} ${PBS_ARRAY_INDEX}
